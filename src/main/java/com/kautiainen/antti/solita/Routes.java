5f7553440ddc6888643f8c9457d51fce4d98b474
Antti Kautiainen <kautsu@elisanet.fi>
2023-05-30 20:56:16 +0300

    fix: Routes of the stations.
    Fixing the routes of the server.

--- src/main/java/com/kautiainen/antti/solita/Routes.java
+++ src/main/java/com/kautiainen/antti/solita/Routes.java
@@ -6,9 +6,16 @@ import io.netty.handler.codec.http.HttpMethod;
 
 import org.restexpress.RestExpress;
 
-
+/**
+ * The routes of the backend serfice.
+ */
 public abstract class Routes
 {
+	/**
+	 * Define the routes of the server.
+	 * @param config The configuration of the server.
+	 * @param server The server instance.
+	 */
 	public static void define(Configuration config, RestExpress server)
     {
 		//TODO: Your routes here...
